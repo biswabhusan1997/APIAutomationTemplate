3b87c43209e4c65c768b01ac5ca3abbc0e207feb
biswabhusan1997 <127864761+biswabhusan1997@users.noreply.github.com>
2023-04-30 10:34:46 +0530

    Add files via upload
    API automation folder structure

---
+++ ATB2xAPIAutomation/README.md
@@ -0,0 +1,7 @@
+# API AUTOMATION FRAMEWORK
+
+#Tech Stack
+
+1.Java
+2. Rest assured
+3. TestNG, 
---
+++ ATB2xAPIAutomation/src/test/java/org/example/test.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import io.restassured.RestAssured;
+
+public class test {
+
+    public static void main(String[] args) {
+
+        RestAssured.given();
+    }
+}
